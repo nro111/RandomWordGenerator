1f44487385900adbe6c01033fe8275e1106c4032
Nick <nickormsby91@gmail.com>
2021-04-28 12:47:52 -0400

    Keeps track of names added and deleted

--- randomwordgenerator/lib/main.dart
+++ randomwordgenerator/lib/main.dart
@@ -21,11 +21,32 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   @override
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = TextStyle(fontSize: 18.0);
+  int _numberOfFavorites = 0;
+  int _numberOfDeletes = 0;
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            new Column(
+              children: [
+                Text("Startup Name Generator"),
+              ],
+            ),
+            new Column(
+              children: [
+                Text("Favorites"),
+                Text(_numberOfFavorites.toString())
+              ],
+            ),
+            new Column(
+              children: [Text("Deletes"), Text(_numberOfDeletes.toString())],
+            )
+          ],
+        ),
       ),
       body: _buildSuggestions(),
     );
@@ -46,10 +67,46 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Wrap(
+        spacing: 12,
+        children: [
+          IconButton(
+            icon: new Icon(Icons.delete),
+            onPressed: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(pair);
+                  _numberOfFavorites--;
+                }
+                _suggestions.remove(pair);
+                _numberOfDeletes++;
+              });
+            },
+          ),
+          IconButton(
+            icon: new Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_outline,
+              color: Colors.red,
+            ),
+            onPressed: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(pair);
+                  _numberOfFavorites--;
+                } else {
+                  _saved.add(pair);
+                  _numberOfFavorites++;
+                }
+              });
+            },
+          ),
+        ],
       ),
     );
   }
